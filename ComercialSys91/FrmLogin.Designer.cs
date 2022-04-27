236cb2cd3f967191022e3043d925cd4ac863377f
JHONATA BATISTA SANTOS <jho.bat9@gmail.com>
2022-04-27 11:37:08 -0300

    é

--- ComercialSys91/FrmLogin.Designer.cs
+++ ComercialSys91/FrmLogin.Designer.cs
@@ -34,7 +34,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -87,22 +87,22 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "Senha";
             // 
-            // label3
+            // lblA
             // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(56, 147);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.ForeColor = System.Drawing.Color.Red;
+            this.lblA.Location = new System.Drawing.Point(56, 147);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(0, 15);
+            this.lblA.TabIndex = 6;
             // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(308, 241);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblA);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
@@ -127,6 +127,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblA;
     }
 }
